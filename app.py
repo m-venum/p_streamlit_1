a0c648a60afaa21b23dc9d19953f6f7527e25a3d
m-venum <79003222+m-venum@users.noreply.github.com>
2024-12-22 17:57:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-import streamlit as st
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
 
-st.title("Hello World")
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+
+if st.checkbox('Show dataframe 1'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
